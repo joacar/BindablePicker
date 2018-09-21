38cd52021be7908e934d91596b55d901f54dd23b
Joakim Carselind <j.carselind@gmail.com>
2018-09-21 11:22:53 +0200

    Upgrade test to NET Standard style

---
+++ tests/JPC.BindablePicker.Tests/ContextFixture.cs
@@ -0,0 +1,26 @@
+﻿namespace JPC.BindablePicker.Tests
+{
+    internal class ContextFixture
+    {
+        public class NestedClass
+        {
+            public string Nested { get; set; }
+        }
+
+        public NestedClass Nested { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public string ComplexName { get; set; }
+
+        public ContextFixture(string displayName, string complexName)
+        {
+            DisplayName = displayName;
+            ComplexName = complexName;
+        }
+
+        public ContextFixture()
+        {
+        }
+    }
+}
